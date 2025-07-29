086c4f1b6133c54b538eafe713f48c44d30e27e1
NXB India <suport@nxbindia.com>
2025-07-29 16:41:14 +0530

    user model update

--- app/models/User.ts
+++ app/models/User.ts
@@ -1,13 +1,40 @@
 import { DateTime } from 'luxon'
+import hash from '@adonisjs/core/services/hash'
+import { compose } from '@adonisjs/core/helpers'
 import { BaseModel, column } from '@adonisjs/lucid/orm'
+import { withAuthFinder } from '@adonisjs/auth/mixins/lucid'
+import { DbRememberMeTokensProvider } from '@adonisjs/auth/session'
+const AuthFinder = withAuthFinder(() => hash.use('scrypt'), {
+  uids: ['email'],
+  passwordColumnName: 'password',
+})
 
-export default class User extends BaseModel {
+export default class User extends compose(BaseModel, AuthFinder) {
+  static rememberMeTokens = DbRememberMeTokensProvider.forModel(User)
   @column({ isPrimary: true })
   declare id: number
-  
+
+  @column({ serializeAs:'full_name' })
+  declare fullName: string | null
+
+  @column()
+  declare email: string
+
+  @column()
+  declare mobile: string
+
+  @column()
+  declare role: string
+
+  @column()
+  declare status: string
+
+  @column({ serializeAs: null })
+  declare password: string
+
   @column.dateTime({ autoCreate: true })
   declare createdAt: DateTime
 
   @column.dateTime({ autoCreate: true, autoUpdate: true })
-  declare updatedAt: DateTime
+  declare updatedAt: DateTime | null
 }
